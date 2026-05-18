7364d1a114e8c9e52f70835cad838910b3aa7ce6
dan <69502994+ggasa@users.noreply.github.com>
2026-05-18 15:43:47 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        unique_s = set(s)
+        unique_t = set(t)
+        combined = s+t
+        unique_st = set(combined)
+        
+        count = 0
+
+        for l in unique_st:
+            print(l) 
+            original_count = s.count(f'{l}')
+            combined_count = combined.count(f'{l}')
+            if unique_s == unique_t:
+                if original_count*2 != combined_count:
+                    output = False
+                else:
+                    output = True
+            else:
+                output = False
+        return output
