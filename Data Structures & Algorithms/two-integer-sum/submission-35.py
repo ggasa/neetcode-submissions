b4720e5407217f26c5d0bdb16f342f54c10e6b92
dan <69502994+ggasa@users.noreply.github.com>
2026-05-18 20:01:38 -0400

    Add: two-integer-sum - submission-35

---
+++ Data Structures & Algorithms/two-integer-sum/submission-35.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        memory = {}
+
+        for i,num in enumerate(nums):
+            rest = target - num
+            if rest in memory:
+                out = [memory[rest],i]
+            memory[num] = i
+
+        return out
