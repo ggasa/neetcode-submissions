341805040215be7e5b4d214f2b448b3de1cd06e1
dan <69502994+ggasa@users.noreply.github.com>
2026-05-21 16:44:19 -0400

    Add: is-palindrome - submission-24

---
+++ Data Structures & Algorithms/is-palindrome/submission-24.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s= s.lower()
+        s = [l for l in s if l.isalnum()]
+        print(s)
+
+        # In this problem you actually care about the order...can't use hashmapping
+        for i,l in enumerate(s):
+            print(f"index: {i}, value: {l}")
+            print(f"m index: {-i}, value: {s[-i-1]}")
+            if l != s[-i-1]:
+                return False
+        return True
+
+        return out
